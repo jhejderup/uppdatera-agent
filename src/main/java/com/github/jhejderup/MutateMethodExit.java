d2ed673140437bd5cc64ac550e341bb2417d0544
Joseph Hejderup <jhejderup@users.noreply.github.com>
2019-08-11 12:09:32 +0200

    apache lang3 StringUtils replace normal

--- src/main/java/com/github/jhejderup/MutateMethodExit.java
+++ src/main/java/com/github/jhejderup/MutateMethodExit.java
@@ -31,9 +31,10 @@ public class MutateMethodExit extends ClassVisitor {
             return mv;
 
         Type[] args =  Type.getArgumentTypes(desc);
-        if(args.length == 2
-                && args[0].getDescriptor().equals("Ljava/lang/Iterable;")
-                && args[1].getDescriptor().equals("Ljava/lang/String;")){
+        if(args.length == 3
+                && args[0].getDescriptor().equals("Ljava/lang/String;")
+                && args[1].getDescriptor().equals("Ljava/lang/String;")
+                && args[2].getDescriptor().equals("Ljava/lang/String;")){
             return new MutateReturn(mv, access, name, desc);
         } else {
             return mv;
@@ -49,72 +50,8 @@ public class MutateMethodExit extends ClassVisitor {
 
         @Override
         protected void onMethodEnter() {
-            Type[] args = Type.getArgumentTypes(this.methodDesc);
-            for (int i = 0; i < args.length; i++) {
-                if (args[i].getSort() == Type.OBJECT && args[i].getDescriptor().equals("Ljava/lang/Iterable;")) {
-                    // Iterable parameter
-                    int off = (this.methodAccess | Opcodes.ACC_STATIC) == 0 ? 0 : 1;
-                    int param = 0; //i + off;
-                    System.out.println("Parameter: " + param);
-                    //New var to store iterator
-                    Type it_type = Type.getType("Ljava/util/Iterator;");
-                    int it_id = newLocal(it_type);
-                    System.out.println("ITR_ID: " + it_id );
 
-                    // New var to store array
-                    Type arr_type = Type.getType("Ljava/util/Collection;");
-                    int arr_id = newLocal(arr_type);
-                    System.out.println("ARR_ID: " + arr_id );
-
-                    //New generic placeholder var
-                    int gen_id = newLocal(Type.getType("Ljava/lang/Object;"));
-                    System.out.println("LOOP_ID: " + gen_id);
-
-                    // Create new array and store it
-                    visitTypeInsn(NEW, "java/util/ArrayList");
-                    visitInsn(DUP);
-                    visitMethodInsn(INVOKESPECIAL, "java/util/ArrayList", "<init>", "()V", false);
-                    visitVarInsn(ASTORE, arr_id);
-
-                    // Load iterator parameter and store it
-                    visitVarInsn(ALOAD, param);
-                    visitMethodInsn(INVOKEINTERFACE, "java/lang/Iterable", "iterator", "()Ljava/util/Iterator;", true);
-                    visitVarInsn(ASTORE, it_id);
-
-                    //Load iterator and check hasNext
-                    Label doLoop = new Label();
-                    visitLabel(doLoop);
-                    visitVarInsn(ALOAD, it_id);
-                    visitMethodInsn(INVOKEINTERFACE, "java/util/Iterator", "hasNext", "()Z", true);
-                    Label doneLoop = new Label();
-
-                    //loop and add elements to a new list
-                    visitJumpInsn(IFEQ, doneLoop);
-                    visitVarInsn(ALOAD, it_id);
-                    visitMethodInsn(INVOKEINTERFACE, "java/util/Iterator", "next", "()Ljava/lang/Object;", true);
-                    visitVarInsn(ASTORE, gen_id);
-                    visitVarInsn(ALOAD, arr_id);
-                    visitVarInsn(ALOAD, gen_id);
-                    visitMethodInsn(INVOKEINTERFACE, "java/util/Collection", "add", "(Ljava/lang/Object;)Z", true);
-                    visitInsn(POP);
-                    visitJumpInsn(GOTO, doLoop);
-
-                    //done with adding all elements
-                    //duplicate array with the same elements
-                    visitLabel(doneLoop);
-                    //shuffle the list
-                    visitVarInsn(ALOAD, arr_id);
-                    visitMethodInsn(INVOKESTATIC, "java/util/Collections", "shuffle", "(Ljava/util/List;)V", false);
-                    //store the array in place of the iterator
-                    visitVarInsn(ALOAD, arr_id);
-                    visitVarInsn(ASTORE, param);
-                } else {
-                    System.out.println("Outside: " + args[i].getDescriptor() + "Sort: " + args[i].getSort());
-                }
-            }
         }
-
-
 
         @Override
         public void visitMaxs(int maxStack, int maxLocals) {
--- src/main/java/com/github/jhejderup/Transformer.java
+++ src/main/java/com/github/jhejderup/Transformer.java
@@ -18,7 +18,7 @@ public class Transformer implements ClassFileTransformer {
             ClassReader reader = new ClassReader(classfileBuffer);
             ClassWriter writer = new ClassWriter(reader, ClassWriter.COMPUTE_FRAMES);
           //  MethodReplacer visitor = new MethodReplacer(writer, "join", className);
-           MutateMethodExit visitor = new MutateMethodExit(writer, "join", className);
+           MutateMethodExit visitor = new MutateMethodExit(writer, "replace", className);
             reader.accept(visitor, ClassReader.EXPAND_FRAMES);
             return writer.toByteArray();
         }
