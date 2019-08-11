4bedca496f6887d74d6a93b124d5d1251e416141
Joseph Hejderup <jhejderup@users.noreply.github.com>
2019-08-11 12:57:56 +0200

    apache http normal case

--- src/main/java/com/github/jhejderup/MutateMethodExit.java
+++ src/main/java/com/github/jhejderup/MutateMethodExit.java
@@ -13,6 +13,27 @@ import static org.objectweb.asm.Opcodes.*;
 public class MutateMethodExit extends ClassVisitor {
 
     private final String hotMethodName, hotClassName;
+    private static Map<Integer, Integer> opcodeMap = new HashMap<Integer, Integer>();
+
+    static {
+        opcodeMap.put(IF_ACMPEQ, IF_ACMPNE);
+        opcodeMap.put(IF_ACMPNE, IF_ACMPEQ);
+        opcodeMap.put(IF_ICMPEQ, IF_ICMPNE);
+        opcodeMap.put(IF_ICMPGE, IF_ICMPLT);
+        opcodeMap.put(IF_ICMPGT, IF_ICMPLE);
+        opcodeMap.put(IF_ICMPLE, IF_ICMPGT);
+        opcodeMap.put(IF_ICMPLT, IF_ICMPGE);
+        opcodeMap.put(IF_ICMPNE, IF_ICMPEQ);
+        opcodeMap.put(IFEQ, IFNE);
+        opcodeMap.put(IFGE, IFLT);
+        opcodeMap.put(IFGT, IFLE);
+        opcodeMap.put(IFLE, IFGT);
+        opcodeMap.put(IFLT, IFGE);
+        opcodeMap.put(IFNE, IFEQ);
+        opcodeMap.put(IFNONNULL, IFNULL);
+        opcodeMap.put(IFNULL, IFNONNULL);
+    }
+
 
     public MutateMethodExit(ClassWriter cw, String methodName, String clazzName) {
         super(Opcodes.ASM5, cw);
@@ -29,12 +50,8 @@ public class MutateMethodExit extends ClassVisitor {
 
         if (!name.equals(this.hotMethodName))
             return mv;
-
         Type[] args =  Type.getArgumentTypes(desc);
-        if(args.length == 3
-                && args[0].getDescriptor().equals("Ljava/lang/String;")
-                && args[1].getDescriptor().equals("Ljava/lang/String;")
-                && args[2].getDescriptor().equals("Ljava/lang/String;")){
+        if(args.length == 0){
             return new MutateReturn(mv, access, name, desc);
         } else {
             return mv;
@@ -48,14 +65,31 @@ public class MutateMethodExit extends ClassVisitor {
             super(Opcodes.ASM5, mv, access, name, desc);
         }
 
-        @Override
-        protected void onMethodEnter() {
 
-        }
+//        @Override
+//        public void visitJumpInsn(int opcode, Label label) {
+//            super.visitJumpInsn(opcodeMap.get(opcode), label);
+//        }
+
 
         @Override
         public void visitMaxs(int maxStack, int maxLocals) {
             super.visitMaxs(0, 0);
+        }
+
+        @Override
+        public void visitEnd() {
+            super.visitEnd();
+        }
+
+        @Override
+        public AnnotationVisitor visitAnnotation(String desc, boolean visible) {
+            return super.visitAnnotation(desc, visible);
+        }
+
+        @Override
+        public void visitParameter(String name, int access) {
+            super.visitParameter(name, access);
         }
 
     }
--- src/main/java/com/github/jhejderup/Transformer.java
+++ src/main/java/com/github/jhejderup/Transformer.java
@@ -14,11 +14,11 @@ public class Transformer implements ClassFileTransformer {
                             ProtectionDomain protectionDomain, byte[] classfileBuffer)
             throws IllegalClassFormatException {
 
-        if (className.equals("org/apache/commons/lang3/StringUtils")) {
+        if (className.equals("org/apache/http/impl/client/HttpClientBuilder")) {
             ClassReader reader = new ClassReader(classfileBuffer);
             ClassWriter writer = new ClassWriter(reader, ClassWriter.COMPUTE_FRAMES);
-            MethodReplacer visitor = new MethodReplacer(writer, "replace", className);
-         //  MutateMethodExit visitor = new MutateMethodExit(writer, "replace", className);
+          //  MethodReplacer visitor = new MethodReplacer(writer, "replace", className);
+            MutateMethodExit visitor = new MutateMethodExit(writer, "build", className);
             reader.accept(visitor, ClassReader.EXPAND_FRAMES);
             return writer.toByteArray();
         }
