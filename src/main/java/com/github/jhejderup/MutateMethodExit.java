3e93dbca33a338ab0dba963b25b465c0c709d73f
Joseph Hejderup <jhejderup@users.noreply.github.com>
2019-08-11 17:33:09 +0200

    jsoup text trivial

--- src/main/java/com/github/jhejderup/MutateMethodExit.java
+++ src/main/java/com/github/jhejderup/MutateMethodExit.java
@@ -13,6 +13,26 @@ import static org.objectweb.asm.Opcodes.*;
 public class MutateMethodExit extends ClassVisitor {
 
     private final String hotMethodName, hotClassName;
+    private static Map<Integer, Integer> opcodeMap = new HashMap<>();
+
+    static {
+        opcodeMap.put(Opcodes.IF_ACMPEQ, Opcodes.IF_ACMPNE);
+        opcodeMap.put(Opcodes.IF_ACMPNE, Opcodes.IF_ACMPEQ);
+        opcodeMap.put(Opcodes.IF_ICMPEQ, Opcodes.IF_ICMPNE);
+        opcodeMap.put(Opcodes.IF_ICMPGE, Opcodes.IF_ICMPLT);
+        opcodeMap.put(Opcodes.IF_ICMPGT, Opcodes.IF_ICMPLE);
+        opcodeMap.put(Opcodes.IF_ICMPLE, Opcodes.IF_ICMPGT);
+        opcodeMap.put(Opcodes.IF_ICMPLT, Opcodes.IF_ICMPGE);
+        opcodeMap.put(Opcodes.IF_ICMPNE, Opcodes.IF_ICMPEQ);
+        opcodeMap.put(Opcodes.IFEQ, Opcodes.IFNE);
+        opcodeMap.put(Opcodes.IFGE, Opcodes.IFLT);
+        opcodeMap.put(Opcodes.IFGT, Opcodes.IFLE);
+        opcodeMap.put(Opcodes.IFLE, Opcodes.IFGT);
+        opcodeMap.put(Opcodes.IFLT, Opcodes.IFGE);
+        opcodeMap.put(Opcodes.IFNE, Opcodes.IFEQ);
+        opcodeMap.put(Opcodes.IFNONNULL, Opcodes.IFNULL);
+        opcodeMap.put(Opcodes.IFNULL, Opcodes.IFNONNULL);
+    }
 
     public MutateMethodExit(ClassWriter cw, String methodName, String clazzName) {
         super(Opcodes.ASM5, cw);
@@ -44,6 +64,11 @@ public class MutateMethodExit extends ClassVisitor {
 
         MutateReturn(final MethodVisitor mv, final int access, final String name, final String desc) {
             super(Opcodes.ASM5, mv, access, name, desc);
+        }
+
+        @Override
+        public void visitJumpInsn(int opcode, Label label) {
+            super.visitJumpInsn(opcodeMap.get(opcode),label);
         }
 
         @Override
